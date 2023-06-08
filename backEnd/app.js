7cfd9633a95bc2139077e343d6c6d9856e876bd7
gusdnr205 <gusdnr205@gmail.com>
2023-06-08 12:08:48 +0900

    d

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,17 +5,22 @@ const session = require("express-session");
 const cors = require("cors")
 
 const app = express();
-const { sequelize } = require("./models")
+const { sequelize } = require("./models");
+const { name } = require("ejs");
 
 app.use(express.urlencoded({extended:false}))
 
 // 이미지 파일을 등록할 폴더 경로
 app.use(express.static(path.join(__dirname,"imege")))
 
+// app.use(cors({
+//     origin: "http://127.0.0.1:5501",     // 각자 html 라이브서버 열어 url이 같은지 확인
+//     credentials : true 
+// }))
 app.use(cors({
-    origin: "http://127.0.0.1:5500",     // 각자 html 라이브서버 열어 url이 같은지 확인
-    credentials : true 
-}))
+    origin:"http://127.0.0.1:5501",
+}
+));
 
 app.use(session({
     secret : process.env.REFRESH_TOKEN_KEY,
@@ -29,6 +34,27 @@ sequelize.sync({forse : false}).then(()=>{
     console.log(err)
 })
 
-app.listen(4000, ()=>{
+// const server에 app 변수할당
+const server=app.listen(4000, ()=>{
     console.log("서버열림")
+})
+
+const io=socketio(server);
+
+io.on("connection",(socket)=>{
+    console.log("유저 접속");
+
+    socket.on("chat message", (msg) => {
+        console.log("받은 메시지: " + msg);
+    
+        // 모든 클라이언트에게 메시지를 전송
+        io.emit("chat message", msg);
+      });
+    
+      // 클라이언트가 연결을 끊었을 때의 처리
+      socket.on("disconnect", () => {
+        console.log("클라이언트가 연결을 끊었습니다.");
+      });
+
+
 })

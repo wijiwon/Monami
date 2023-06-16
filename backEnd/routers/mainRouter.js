6fe3c7210f646d8b166caada48b42beafb21724a
youdonghee <0103donghee@naver.com>
2023-06-16 14:03:16 +0900

    wuthbanjang14

--- backEnd/routers/mainRouter.js
+++ backEnd/routers/mainRouter.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { mainInfo } = require("../controllers/mainControllers");
 const { islogin } = require("../middleware/isLogin");
 
-router.get('/',islogin,(req,res)=>{
+router.get('/',(req,res)=>{
   res.sendFile("/home/ubuntu/frontEnd/main.html");
 });
 

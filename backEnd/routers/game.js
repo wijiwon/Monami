03414df35bac6baa628f8f9c724acb783c2662cf
gusdnr205 <gusdnr205@gmail.com>
2023-06-17 15:33:08 +0900

    dev

--- backEnd/routers/game.js
+++ backEnd/routers/game.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const { islogin } = require('../middleware/isLogin')
+const { isRoom } = require("../middleware/isRoom")
+const { DrawingAdd, viewVideo } = require('../controllers/gamePlay')
 
-router.get("/");
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
+router.post('/', islogin, isRoom, upload.single('file'), DrawingAdd)
 
+router.get('/viewVideo',viewVideo)
 
 module.exports = router;

2616bca28db5c4556f41a2cb1a537232ea41a082
youdonghee <0103donghee@naver.com>
2023-06-17 17:15:31 +0900

    wuthbanjang40

--- backEnd/routers/gameStartRouter.js
+++ backEnd/routers/gameStartRouter.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const { islogin } = require("../middleware/isLogin");
+const { gameStart } = require("../controllers/gameStartControlloer");
 
-router.get('/', islogin, (req, res) => {
-  // 인증된 사용자에게만 접근이 허용됩니다.
-  res.sendFile("/home/ubuntu/frontEnd/game.html");
-});
+router.get('/', islogin, gameStart);
 
 module.exports = router;

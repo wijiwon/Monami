b636933730de042b9bffe6ba1e0e2b9785b53958
youdonghee <0103donghee@naver.com>
2023-06-17 16:38:34 +0900

    wuthbanjang38

--- backEnd/middleware/isLogin.js
+++ backEnd/middleware/isLogin.js
@@ -3,30 +3,21 @@ const dot = require("dotenv").config();
 const cookieParser = require('cookie-parser');
 
 exports.islogin = async (req, res, next) => {
-  console.log("세션에 토큰임당",req.session.access_token);
+  console.log("Assign token to session", req.session.access_token);
 
-  // 무헌이 오빠가 만든 기적의 코드^^
-  // 유효검사
   if (!req.session.access_token) {
-    console.log("토큰 없졍");
-    // return res.status(401).send("토큰 없어");
-    req.isAuthenticated = false;
-  }else {
+    console.log("Token removed");
+    // return res.status(401).send("토큰없음");
+  } else {
     jwt.verify(req.session.access_token, process.env.ACCESS_TOKEN_KEY, (err, decode) => {
       if (err) {
-        console.log("토큰 유효검사 에러 미들웨어임", err);
-        // return res.send("다시 로그인 해주세욥");
-        req.isAuthenticated = false;
-        
+        console.log("Token 유효검사 error middleware", err);
+        // return res.send("다시 로그인 해주세요");
       } else {
-        console.log("디코드 됨",decode);
+        console.log("decoded", decode);
         req.decode = decode;
-        
-        req.isAuthenticated = true;
       }
-    })
+    });
   }
   next();
-  
-  
-}
+};
---
+++ backEnd/routers/gameStartRouter.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { islogin } = require("../middleware/isLogin");
+
+router.get('/', islogin, (req, res) => {
+  // 인증된 사용자에게만 접근이 허용됩니다.
+  res.sendFile("/home/ubuntu/frontEnd/game.html");
+});

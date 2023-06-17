0ef8a63c14f4dcce12992c484dc6a9695786cfb7
youdonghee <0103donghee@naver.com>
2023-06-17 15:40:41 +0900

    wuthbanjang31

--- backEnd/routers/gameStartRouter.js
+++ backEnd/routers/gameStartRouter.js
@@ -1,9 +1,9 @@
-const router = reqire("express").Router();
-// const { }
+// const router = reqire("express").Router();
+// // const { }
 
-// router.get('/',islogin,(req,res)=>{
-//   res.sendFile("/home/ubuntu/frontEnd/gamestart.html");
-// });
-router.get('/',);
+// // router.get('/',islogin,(req,res)=>{
+// //   res.sendFile("/home/ubuntu/frontEnd/gamestart.html");
+// // });
+// router.get('/',);
 
-module.exports = router;
+// module.exports = router;

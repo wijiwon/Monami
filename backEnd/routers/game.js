4f98e3a1aee30d81831c3687996ace8b6e1a8d9d
wijiwon <{dnlwldnjs5@gmail.com}>
2023-06-19 17:54:53 +0900

    게임 초안

--- backEnd/routers/game.js
+++ backEnd/routers/game.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { islogin } = require('../middleware/isLogin')
 const { isRoom } = require("../middleware/isRoom")
-const { DrawingAdd, viewVideo, firstQuestionInput, DrawQueUpdate, QuestionView } = require('../controllers/gamePlay')
+const { DrawingAdd, viewVideo, firstQuestionInput, DrawQueUpdate, QuestionView, TwoQuestionInput } = require('../controllers/gamePlay')
 
 const multer = require('multer');
 const storage = multer.memoryStorage();
@@ -11,10 +11,13 @@ const upload = multer({ storage: storage });
 router.post('/', islogin, isRoom, upload.single('file'), DrawingAdd)
 
 // 저장된 그림을 video로 보여줌 
-router.get('/viewVideo',viewVideo)
+router.post('/viewVideo',viewVideo)
 
-// 입력된 제시어를 db에 저장
+// 첫 번째 입력된 제시어를 db에 저장
 router.post('/InputQuestion', islogin, isRoom, firstQuestionInput)
+
+// 두 번째 입력된 제시어를 db에 저장
+router.post('/TwoQuestionInput', islogin, isRoom, TwoQuestionInput)
 
 // 그림이 저장되면 해당 그림의 id를 해당 제시어의 content에 저장
 router.post('/DrawQueUpdate', islogin, isRoom, DrawQueUpdate)
